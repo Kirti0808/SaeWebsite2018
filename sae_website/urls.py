f21b95f6c13179bfcb804d2b09ff6274f3b6e132
Avinash11a <36043246+Avinash11a@users.noreply.github.com>
2018-12-29 03:33:14 +0530

    updaed urls.py of saewebsite2018
    added index extension

--- sae_website/urls.py
+++ sae_website/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('index.urls')),
+    path('index/', include('index.urls')),
     path('contact/', include('contact.urls')),
     path('announcements/', include('announcements.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
