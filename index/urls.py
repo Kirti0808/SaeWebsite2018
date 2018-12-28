95d8b361c62b8de5d6d314a87205db26aa5166a2
Avinash11a <36043246+Avinash11a@users.noreply.github.com>
2018-12-29 03:28:56 +0530

    updated urls.py
    added link to allteam.html

--- index/urls.py
+++ index/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('events1',views.events,name='events1')
+    path('events1',views.events,name='events1'),
+     path('allteam/',views.allteam,name='allteam'),
     ]
 #from django.conf.urls import url
 
