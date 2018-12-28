0979ef379bfca249b5aeca506f0a703b84be92a1
Avinash11a <36043246+Avinash11a@users.noreply.github.com>
2018-12-29 03:30:07 +0530

    updated views.py

--- index/views.py
+++ index/views.py
@@ -4,4 +4,7 @@ def index(request):
     return render(request,'index/home.html')
 def events(request):
     return render(request,'index/events.html',{})
+def allteam(request):
+    return render(request,'index/allteam.html',{})
+
     
