8977924709ec7cf5825babbf40088dad0ad48e50
kirti0808 <gupta.kirti0808@gmail.com>
2019-02-07 20:40:14 +0530

    updated

--- sae_website/settings.py
+++ sae_website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import os.path
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'oxa#lxae&vh#02b_#2&s9(&uw)xlxy_#b8+geknu0k&2oqc#co'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['saewebsite.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -132,3 +133,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'sae_website/media')
 
 STATICFILES_DIRS = ( os.path.join('static'), )
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
